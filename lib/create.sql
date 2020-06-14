ab8017ac8b34682ed4f17b47a66e9f2d533490c8
Steve Womble <swomble@aol.com>
2020-06-14 20:16:27 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE  project (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal REAL, start_date TEXT, end_date TEXT);
+CREATE TABLE  projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal REAL, start_date TEXT, end_date TEXT);
